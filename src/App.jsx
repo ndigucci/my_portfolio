45595a0583f375ce8a2820a54db677b3b8735df3
ISHIMWE boncoeur <ishimweboncoeur@gmail.com>
2026-01-07 15:45:56 +0200

    adding ascroll to top button

--- src/App.jsx
+++ src/App.jsx
@@ -727,7 +727,7 @@ function App() {
         </section>
       </RevealOnScroll>
       {showScrollBtn && (
-        <button onClick={scrollToTop} className="circle fixed bottom-8 right-8 w-10 h-10 bg-cyan-600 text-white flex items-center justify-center rounded-full shadow-lg hover:bg-cyan-500 transition-transform hover:-translate-y-1 z-50">
+        <button onClick={scrollToTop} className="animated-bounce fixed bottom-8 right-8 bg-cyan-600 text-white p-4 rounded-full shadow-lg hover:bg-cyan-500 transition-transform hover:-translate-y-1">
           ↑
         </button>
       )}
